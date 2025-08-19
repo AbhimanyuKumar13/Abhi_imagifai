a006633a7d48a5ff851e603b7f161f0b5b4b7494
AbhimanyuKumar13 <ak6547765@gmail.com>
2025-08-19 17:16:38 +0530

    samesite

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -8,7 +8,8 @@ export const sendToken = async (user, statusCode, message, res) => {
     .cookie("token", token, {
       expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      secure: true,   
+      secure: true,  
+      sameSite: "none",
     })
     .json({
       success: true,
